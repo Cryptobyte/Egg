e40b21be4f3b7fd28644b9d4e28a3cdbcdb1e9b9
Spyda <cryptobyte1134@gmail.com>
2018-04-01 03:12:44 -0400

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+### Egg Entity Tracker for Forge
+This mod simply draws a red line between the player and eggs within a large radius. The plugin was designed as a proof of concept "_cheat_" for Easter egg hunt style games on popular servers.
+
+### Limitations
+Minecraft seems to only render items within a very short distance of the player (I could be wrong, this is my first Forge mod) which means that no matter what the `RADIUS` variable is set to it won't show all eggs in that radius.
+
+### Warning
+This mod will probably get you banned from whatever server you use it on if you are discovered with it. I wrote this just as a proof of concept and have not used it on any public servers. __Use at your own risk!__
---
+++ src/main/java/org/lawlsec/egg/egg/egg.java
@@ -0,0 +1,98 @@
+package org.lawlsec.egg.egg;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.Item;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraftforge.client.event.DrawBlockHighlightEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.opengl.GL11;
+
+import java.awt.*;
+import java.util.List;
+
+@Mod(modid = egg.MOD_ID, name = egg.MOD_NAME, version = egg.VERSION)
+@Mod.EventBusSubscriber
+public class egg {
+
+    public static final String MOD_ID = "egg";
+    public static final String MOD_NAME = "egg";
+    public static final String VERSION = "1.0";
+
+    public static final int RADIUS = 1024;
+
+    @Mod.Instance(MOD_ID)
+    public static egg INSTANCE;
+
+    @Mod.EventHandler
+    public void preinit(FMLPreInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public void onDrawBlockHighlightEvent(DrawBlockHighlightEvent event) {
+        List<Entity> entities = event.getPlayer().world.getEntitiesWithinAABB(
+            Entity.class,
+            new AxisAlignedBB(
+                event.getPlayer().getPosition().getX() - RADIUS,
+                event.getPlayer().getPosition().getY() - RADIUS,
+                event.getPlayer().getPosition().getZ() - RADIUS,
+                event.getPlayer().getPosition().getX() + RADIUS,
+                event.getPlayer().getPosition().getY() + RADIUS,
+                event.getPlayer().getPosition().getZ() + RADIUS
+            )
+        );
+
+        GL11.glPushAttrib(GL11.GL_ENABLE_BIT);
+        GL11.glDisable(GL11.GL_CULL_FACE);
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glTranslated(
+            -event.getPlayer().getPosition().getX(),
+            -event.getPlayer().getPosition().getY(),
+            -event.getPlayer().getPosition().getZ()
+        );
+
+        Color c = new Color(255, 0, 0, 150);
+        GL11.glColor4d(c.getRed(), c.getGreen(), c.getBlue(), c.getAlpha());
+        GL11.glLineWidth(3);
+        GL11.glDepthMask(false);
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder bufferBuilder = tessellator.getBuffer();
+        bufferBuilder.begin(GL11.GL_LINES, DefaultVertexFormats.POSITION_COLOR);
+
+        entities.forEach(entity -> {
+            if (entity.getName().equals("item.item.egg")) {
+                bufferBuilder.pos(
+                    event.getPlayer().getPosition().getX(),
+                    event.getPlayer().getPosition().getY(),
+                    event.getPlayer().getPosition().getZ()
+
+                ).color(c.getRed(), c.getGreen(), c.getBlue(), c.getAlpha()).endVertex();
+
+                bufferBuilder.pos(entity.posX, entity.posY, entity.posZ).color(c.getRed(), c.getGreen(), c.getBlue(), c.getAlpha()).endVertex();
+            }
+        });
+
+        tessellator.draw();
+
+        GL11.glDepthMask(true);
+        GL11.glPopAttrib();
+    }
+}
